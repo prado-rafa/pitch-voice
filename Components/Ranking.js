5b3ac7ac6e39babe751b38df413e146bfd40f6b4
menezesmalu <mlmv@cin.ufpe.br>
2018-05-24 23:25:25 -0300

    Feature de ranking básica

--- Components/Ranking.js
+++ Components/Ranking.js
@@ -3,25 +3,142 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import PreGame from './PreGame'
 import Game from './Game'
 
-export default class Ranking extends React.Component{
-    render(){
+
+class MyButton extends React.Component {
+  render() {
       return (
-        <View style={styles.container}>
-          <Text style={styles.welcome}>Ranking</Text>
           <Button
-            onPress = {() => this.props.navigation.navigate('PreGame')}
-            title = 'Novo Jogo'
-            color = '#23BAA7'
+              onPress = {this.props.onPress}
+              title = {this.props.title}
+              color = '#23BAA7'
           />
-          <Button
-            onPress = {() => this.props.navigation.navigate('Game')}
-            title = 'Próxima Partida'
-            color = '#23BAA7'
-          />
-        </View>
       );
-    }
   }
+}
+
+export default class Ranking extends React.Component{
+    constructor(props) {
+      super(props) 
+        this.state = { 
+          rank: [],
+          currPoints: 10
+        };
+    }
+    componentDidMount(){
+      const people = [
+        'Malu',
+        'Larícia',
+        'Rossi'
+      ];
+      const rank = people.map (person =>{
+        return {name: person, points: 0};
+      });
+      this.setState({
+        rank: rank
+      })
+    }
+
+    
+    orderPlayer = () => {
+      let rank = this.state.rank;
+      rank.sort = (a, b) => {
+          if (a.points > b.points) {
+            console.log("a")
+              return 1;
+          } else if (b.points > a.points) {
+            console.log("b")
+              return -1;
+          } else {
+              if (a.name > b.name) {
+                console.log("c")
+                  return 1;
+              } else if (b.name > a.name) {
+                console.log("d")
+                  return -1;
+              }
+          }
+          console.log("e")
+          return 0;
+      };
+      console.log("ordenados");
+      console.log(rank);
+      this.setState({
+        rank: rank
+      });
+  }
+  
+    winPoints = (nome) => {
+      let rank = this.state.rank;
+      let currPoints = this.state.currPoints;
+      for (let i in rank) {
+        if(rank[i].name === nome && currPoints >=0) {
+          rank[i].points += currPoints;
+          currPoints--;
+          break;
+        }
+      }
+        let rank2 = rank;
+        rank2.sort((a, b) => {
+          
+             console.log ("entrou")
+             if (a.points < b.points) {
+                 console.log("a")
+                   return 1;
+               } else if (b.points < a.points) {
+                 console.log("b")
+                   return -1;
+               } else {
+                   if (a.name < b.name) {
+                     console.log("c")
+                       return 1;
+                   } else if (b.name < a.name) {
+                     console.log("d")
+                       return -1;
+                   }
+               }
+               console.log("e")
+               return 0;
+           }
+          ); 
+      console.log(rank2);
+      this.setState({
+        rank: rank2,
+        currPoints: currPoints
+      });
+    }
+
+
+  render() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.welcome}>Ranking</Text>
+            <MyButton
+                onPress = {this.orderPlayers}
+                title = 'ordenar'
+            />  
+            <MyButton
+                onPress = {() => this.props.navigation.navigate('PreGame')}
+                title = 'Novo Jogo'
+            />
+            <MyButton
+                onPress = {() => this.props.navigation.navigate('Game')}
+                title = 'Próxima Partida'
+            />
+            {this.state.rank.map((p, i) => {
+                    return (
+                        <View key={i}>
+                            <Text>{p.name + ': ' + p.points}</Text>
+                            <MyButton
+                                onPress = {() => this.winPoints(p.name)}
+                                title = 'alterar'
+                            />
+                        </View>
+                    );
+                })}
+        </View>
+    );
+  }
+}
   
 const styles = StyleSheet.create({
     container: {
@@ -38,5 +155,4 @@ const styles = StyleSheet.create({
       color: '#23BAA7',
     }
     
-  });
-  
+  });    
